bc7a53773d69fe6ac23018eab774334ee576d563
Hubert Ritter <hubert.ritter@iqsoft.com>
2021-11-05 14:23:36 +0100

    Add SpringApplication ConnversionCalculator main

---
+++ src/main/java/com/example/conversioncalculator/ConversionCalculator.java
@@ -0,0 +1,13 @@
+package com.example.conversioncalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionCalculator {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversionCalculator.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/conversioncalculator/ConversionCalculatorTests.java
@@ -0,0 +1,13 @@
+package com.example.conversioncalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionCalculatorTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
